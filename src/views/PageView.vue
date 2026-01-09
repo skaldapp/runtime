3c5ee9510c96a7a793d487ff1cd21bdbde5d3360
Jerry Bruwes <jbruwes@icloud.com>
2026-01-09 17:50:37 +0200

    refactor: improve PageView component structure and formatting

--- src/views/PageView.vue
+++ src/views/PageView.vue
@@ -3,8 +3,7 @@ div(
   v-for="[id, is] in templates",
   :id,
   :key="id",
-  v-element-visibility="[(state)=>{intersecting.set(id,state)},{threshold:0.1}]",
-  un-cloak
+  v-element-visibility="[(state)=>{intersecting.set(id,state)},{threshold:0.1}]"
 )
   div(v-bind="kvNodes[id]?.frontmatter['attrs'] ?? {}")
     component(:is, @vue:mounted="promises.get(id)?.resolve(undefined)")
@@ -17,26 +16,35 @@ import { computed, onUnmounted, toRef, watchEffect } from "vue";
 
 import { mainStore, promiseWithResolvers } from "@/stores/main";
 
-const $these = $toRef(mainStore, "$these"),
-  kvNodes = toRef(sharedStore, "kvNodes"),
-  { intersecting, module, promises } = mainStore;
+/* -------------------------------------------------------------------------- */
+
+const $these = toRef(mainStore, "$these"),
+  kvNodes = toRef(sharedStore, "kvNodes");
+
+/* -------------------------------------------------------------------------- */
+
+const { intersecting, module, promises } = mainStore;
+
+/* -------------------------------------------------------------------------- */
 
 const clear = () => {
     [intersecting, promises].forEach((map) => {
       map.clear();
     });
   },
-  templates = computed(() => new Map($these.map(({ id }) => [id, module(id)])));
+  templates = computed(
+    () => new Map($these.value.map(({ id }) => [id, module(id)])),
+  );
+
+/* -------------------------------------------------------------------------- */
 
 watchEffect(() => {
   clear();
-  $these.forEach(({ id }) => {
+  $these.value.forEach(({ id }) => {
     intersecting.set(id, false);
     promises.set(id, promiseWithResolvers());
   });
 });
 
-onUnmounted(() => {
-  clear();
-});
+onUnmounted(clear);
 </script>
